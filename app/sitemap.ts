03fd4021a7daa58bc238c5ddfad3bc0f261c297b
kdmppulautongahkecamatanbenai-alt <kdmppulautongahkecamatanbenai@gmail.com>
2026-05-16 15:08:15 +0700

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,1 +1,25 @@
+import { MetadataRoute } from 'next';
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://domain-anda.com',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://domain-anda.com',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://domain-anda.com',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+  ];
+}
 
